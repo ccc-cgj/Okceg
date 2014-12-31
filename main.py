aa3b4f4441f1ef6beebe9955c367af5a9ee48a58
mattzjack <mudsharkmk@gmail.com>
2014-12-31 22:55:09 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+def is_num(jerry):
+    try:
+        int(jerry)
+    except:
+        try:
+            float(jerry)
+        except:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+print('\nInteractive Okceg Shell!\npowered by python\n')
+
+while(True):
+    i = input('>')
+
+    if i.lower() == 'exit()':
+        break
+
+    elif i[:6] == 'class(' and i[-1] == ')':
+        c = i[6:-1]
+        if is_num(c):
+            print('number')
+        elif c == 'true' or c == 'false':
+            print('boolean')
+        else:
+            print('string')
+
+    elif i[:5] == 'math(' and i[-1] == ')':
+        c = i[5:-1].split(' ')
+        o = c[0]
+        l = c[1]
+        t = c[2]
+        if l == '+':
+            print(int(o) + int(t))
+        elif l == '-':
+            print(int(o) - int(t))
+        elif l == '*':
+            print(int(o) * int(t))
+        elif l == '/':
+            print(int(o) / int(t))
+
+    else:
+        print('nonsense')
